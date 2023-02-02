6c4f95512c470d3720246de4ca7a7b0263d71013
Xiphe <github@xiphe.net>
2023-02-02 16:09:15 +0100

    feat: more magic 🧙

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,35 @@ import type { EntryContext } from '@remix-run/server-runtime';
 import type { ComponentType } from 'react';
 import { createElement, useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
+import { RemixServer } from '@remix-run/react';
+import { renderToString } from 'react-dom/server';
 
-const RENDER_ON_SERVER = Symbol();
-type HeadComponent = ComponentType<{ [RENDER_ON_SERVER]?: boolean }>;
+type HeadComponent = ComponentType<{
+  __remix_island_render_server?: boolean;
+}> & {
+  __remix_island_id?: string;
+};
 
 let globalMounted = false;
-export function createHead(Comp: ComponentType): HeadComponent {
-  return function Head({ [RENDER_ON_SERVER]: renderOnServer }) {
+export interface CreateHeadOpts {
+  id?: string;
+  cleanup?: boolean;
+}
+export function createHead(
+  Comp: ComponentType,
+  { id = 'remix-island', cleanup = true }: CreateHeadOpts = {},
+): HeadComponent {
+  const Head: HeadComponent = (props) => {
     const [mounted, setMounted] = useState(globalMounted);
     useEffect(() => {
+      if (cleanup) {
+        removeOldHead(Head);
+      }
       globalMounted = true;
       setMounted(true);
     }, []);
 
-    if (!mounted && renderOnServer) {
+    if (!mounted && props.__remix_island_render_server) {
       return createElement(Comp);
     }
 
@@ -25,6 +40,33 @@ export function createHead(Comp: ComponentType): HeadComponent {
 
     return null;
   };
+
+  Head.displayName = 'RemixIslandHead';
+  Head.__remix_island_id = id;
+
+  return Head;
+}
+
+export interface RenderHeadToStringOpts {
+  request: Request;
+  remixContext: EntryContext;
+  Head: HeadComponent;
+}
+
+export function renderHeadToString({
+  request,
+  remixContext,
+  Head,
+}: RenderHeadToStringOpts) {
+  const head = renderToString(
+    createElement(RemixServer, {
+      // @ts-expect-error no idea why ts complains here... ¯\_(ツ)_/¯
+      context: switchRootComponent(remixContext, Head),
+      url: request.url,
+    }),
+  );
+  const id = Head.__remix_island_id;
+  return `<!--${id}-start-->${head}<!--${id}-end-->`;
 }
 
 export function switchRootComponent(
@@ -37,21 +79,20 @@ export function switchRootComponent(
       ...remixContext.routeModules,
       root: {
         ...remixContext.routeModules.root,
-        default: () => createElement(Head, { [RENDER_ON_SERVER]: true }),
+        default: () =>
+          createElement(Head, { __remix_island_render_server: true }),
       },
     },
   };
 }
 
-const DEFAULT_ID = 'remix-head';
-
-export function markForRemoval(content: string, id: string = DEFAULT_ID) {
-  return `<!--${id}-start-->${content}<!--${id}-end-->`;
-}
-
-export function removeMarked(parent: HTMLElement, id: string = DEFAULT_ID) {
+export function removeOldHead(
+  Head: HeadComponent,
+  parent: HTMLElement = document.head,
+) {
   let foundOldHeader = false;
   const nodesToRemove: ChildNode[] = [];
+  const id = Head.__remix_island_id;
   for (const node of parent.childNodes) {
     if (!foundOldHeader && node.nodeName !== '#comment') {
       continue;
