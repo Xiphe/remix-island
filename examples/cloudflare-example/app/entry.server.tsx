fb3f3eeed3e04f74e9a0ae705ffcd42464aba1ed
Aaron Powell <Apowell829@gmail.com>
2023-02-27 13:40:47 +0000

    docs: install remix-island

--- examples/cloudflare-example/app/entry.server.tsx
+++ examples/cloudflare-example/app/entry.server.tsx
@@ -1,21 +1,79 @@
-import type { EntryContext } from "@remix-run/cloudflare";
-import { RemixServer } from "@remix-run/react";
-import { renderToString } from "react-dom/server";
+import type { EntryContext } from '@remix-run/cloudflare';
+import { RemixServer } from '@remix-run/react';
+import { Head } from './root';
+import { renderHeadToString } from 'remix-island';
+
+import { renderToReadableStream } from 'react-dom/server';
 
 export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  remixContext: EntryContext
+  remixContext: EntryContext,
 ) {
-  const markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
+  return handleBrowserRequest(
+    request,
+    responseStatusCode,
+    responseHeaders,
+    remixContext,
+  );
+}
+
+async function handleBrowserRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext,
+) {
+  let didError = false;
+  const readable = await renderToReadableStream(
+    <RemixServer context={remixContext} url={request.url} />,
   );
 
-  responseHeaders.set("Content-Type", "text/html");
+  responseHeaders.set('Content-Type', 'text/html');
+  const stream = new ReadableStream({
+    start(controller) {
+      // Add the HTML head to the response
+      const head = renderHeadToString({ request, remixContext, Head });
+      controller.enqueue(
+        new Uint8Array(
+          new TextEncoder().encode(
+            `<!DOCTYPE html><html><head>${head}</head><body><div id="root">`,
+          ),
+        ),
+      );
 
-  return new Response("<!DOCTYPE html>" + markup, {
-    status: responseStatusCode,
+      const reader = readable.getReader();
+      function read() {
+        reader
+          .read()
+          .then(({ done, value }) => {
+            if (done) {
+              controller.enqueue(
+                new Uint8Array(
+                  new TextEncoder().encode(`</div></body></html>`),
+                ),
+              );
+              controller.close();
+              return;
+            }
+            controller.enqueue(value);
+            read();
+          })
+          .catch((error) => {
+            controller.error(error);
+            readable.cancel();
+          });
+      }
+      read();
+    },
+    cancel() {
+      readable.cancel();
+    },
+  });
+
+  return new Response(stream, {
     headers: responseHeaders,
+    status: didError ? 500 : responseStatusCode,
   });
 }
