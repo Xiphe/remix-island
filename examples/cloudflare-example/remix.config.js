61cfe98dd2f7452f2083191b40c38f737a3a10e5
Aaron Powell <Apowell829@gmail.com>
2023-02-27 13:35:10 +0000

    docs: Create cloudflare example - npx create-remix@latest

---
+++ examples/cloudflare-example/remix.config.js
@@ -0,0 +1,16 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  devServerBroadcastDelay: 1000,
+  ignoredRouteFiles: ["**/.*"],
+  server: "./server.js",
+  serverBuildPath: "functions/[[path]].js",
+  serverConditions: ["worker"],
+  serverDependenciesToBundle: "all",
+  serverMainFields: ["browser", "module", "main"],
+  serverMinify: true,
+  serverModuleFormat: "esm",
+  serverPlatform: "neutral",
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // publicPath: "/build/",
+};
